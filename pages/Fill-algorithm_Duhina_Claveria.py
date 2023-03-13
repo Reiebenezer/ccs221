a2e34a5a3d503b418197f9ae8e03c9abb47548e0
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2023-03-13 19:07:15 +0800

    activity 1

--- pages/Fill-algorithm_Duhina_Claveria.py
+++ pages/Fill-algorithm_Duhina_Claveria.py
@@ -1,22 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import streamlit as st
 
 def plot(two_d_arr:np.array, row, column, color):
 
     two_d_arr[row, column] = color
 
-    plt.imshow(two_d_arr, interpolation='none', cmap='plasma')
-    plt.colorbar()
-    plt.show()
+    # plt.imshow(two_d_arr, interpolation='none', cmap='plasma')
+    # plt.colorbar()
+    # plt.show()
+    st.pyplot(two_d_arr, interpolation='none', cmap='plasma')
 
 def randomizeplot(two_d_arr:np.array):
     for i in range(3):
         for j in range(3):
             two_d_arr[i, j] = int(np.random.randint(0, 6))
 
-    plt.imshow(two_d_arr, interpolation='none', cmap='plasma')
-    plt.colorbar()
-    plt.show()
+    # plt.imshow(two_d_arr, interpolation='none', cmap='plasma')
+    # plt.colorbar()
+    # plt.show()
+
+    st.pyplot(two_d_arr, interpolation='none', cmap='plasma')
+
 
 
 if __name__ == '__main__':
@@ -28,16 +33,12 @@ if __name__ == '__main__':
         for j in range(3):
             two_d_arr[i, j] = 5 if two_d_arr[i, j] == 1 else 0
     
-    while True:
-        randomize = input("Type 1 to randomize all pixel colors. \nType anything else to change specific pixel color\n>>> ")
-        if randomize == '1':
-            randomizeplot(two_d_arr)
-        else:
-            row = int(input("Enter Row (from 1 to 3)\n>>> "))
-            column = int(input("Enter  Column (from 1 to 3)\n>>> "))
-            color = float(input("Enter Color Value (from 0 to 5)\nNOTE: must be integer!\n>>> "))
-            plot(two_d_arr, row-1, column-1, color)
+    if st.button("Click to randomize all pixel colors"):
+        randomizeplot(two_d_arr)
         
-        exit = input("Exit? Y/N: ")
-        if(exit == 'Y' or exit == 'y'):
-            break
+    if st.button("Click to set pixel color"):
+        row = int(st.number_input("Enter Row (from 1 to 3)\n>>> "))
+        column = int(st.number_input("Enter  Column (from 1 to 3)\n>>> "))
+        color = float(st.number_input("Enter Color Value (from 0 to 5)\nNOTE: must be integer!\n>>> "))
+        plot(two_d_arr, row-1, column-1, color)
+        

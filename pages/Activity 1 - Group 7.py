c376df414aee24225c315ea94f482dc9de90d7f4
Reiebenezer <126635968+Reiebenezer@users.noreply.github.com>
2023-03-14 13:41:29 +0800

    Update Activity 1 - Group 7.py

--- pages/Activity 1 - Group 7.py
+++ pages/Activity 1 - Group 7.py
@@ -128,7 +128,7 @@ def MidpointLine2(x1, y1, x2, y2, color, axis):
 
 def main():
     x = int(st.sidebar.number_input("Enter X1: "))
-    y = int(st.sidebar.number_input()("Enter Y1: "))
+    y = int(st.sidebar.number_input("Enter Y1: "))
     xEnd = int(st.sidebar.number_input("Enter X2: "))
     yEnd = int(st.sidebar.number_input("Enter Y2: "))
     color = "b."

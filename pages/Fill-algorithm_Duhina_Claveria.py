3925adf261a18422229a41f890a0c96035c2db83
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2023-03-14 14:16:22 +0800

    s

--- pages/Fill-algorithm_Duhina_Claveria.py
+++ pages/Fill-algorithm_Duhina_Claveria.py
@@ -8,22 +8,20 @@ def plot(two_d_arr:np.array, row, column, color):
 
     two_d_arr[row, column] = color
 
-    # plt.imshow(two_d_arr, interpolation='none', cmap='plasma')
-    # plt.colorbar()
-    # plt.show()
-    st.pyplot(two_d_arr, interpolation='none', cmap='plasma')
+    img = plt.imshow(two_d_arr, interpolation='none', cmap='plasma')
+    img.set_clim([0, 50])
+    plt.colorbar()
+    return img
 
 def randomizeplot(two_d_arr:np.array):
     for i in range(3):
         for j in range(3):
             two_d_arr[i, j] = int(np.random.randint(0, 6))
 
-    # plt.imshow(two_d_arr, interpolation='none', cmap='plasma')
-    # plt.colorbar()
-    # plt.show()
-    img = Image.fromarray(two_d_arr, "RGB")
-    st.image(img, caption = "Random Pixels")
-
+    img = plt.imshow(two_d_arr, interpolation='none', cmap='plasma')
+    img.set_clim([0, 50])
+    plt.colorbar()
+    return img
 
 if __name__ == '__main__':
     two_d_arr = np.array([[1, 0, 1],
@@ -35,11 +33,15 @@ if __name__ == '__main__':
             two_d_arr[i, j] = 5 if two_d_arr[i, j] == 1 else 0
     
     if st.button("Click to randomize all pixel colors"):
-        randomizeplot(two_d_arr)
+        img = randomizeplot(two_d_arr)
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        st.pyplot()
         
     if st.button("Click to set pixel color"):
         row = int(st.number_input("Enter Row (from 1 to 3)\n>>> "))
         column = int(st.number_input("Enter  Column (from 1 to 3)\n>>> "))
         color = float(st.number_input("Enter Color Value (from 0 to 5)\nNOTE: must be integer!\n>>> "))
-        plot(two_d_arr, row-1, column-1, color)
+        img = plot(two_d_arr, row-1, column-1, color)
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        st.pyplot()
         

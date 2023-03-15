8720effb618b256a6124ac60e2f6e4f14e6fd24f
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2023-03-15 16:04:17 +0800

    s

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.title("Welcome to... whatever the heck of a project this is.")
-st.subheader("This is the result of many contributions from various computer scientists and mathematicians around the world.")
+st.subheader("This is the result of many contributions from various computer scientists and mathematicians around the world," +
+             "Gathering information from every possible source and placing them in this heck of a machine that displays random" + 
+             "three-dimensional objects and complete random lines and colors. WHAT A WASTE OF TIME FOR THOSE PEOPLE")

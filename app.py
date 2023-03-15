2fc1907e36d4781b3c92cd126e155d08b721cc33
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2023-03-15 16:04:00 +0800

    s

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.title("Hello")
+st.title("Welcome to... whatever the heck of a project this is.")
+st.subheader("This is the result of many contributions from various computer scientists and mathematicians around the world.")
